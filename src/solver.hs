fef5b41bc68dd728d8ef53fc43bddcb47d6e3350
Unknown <modzel1001@gmail.com>
2018-12-13 23:14:27 +0100

    indexy literki w planszy

--- src/solver.hs
+++ src/solver.hs
@@ -1,37 +1,33 @@
 import System.IO
-
+import Data.List
 
 --data Coords = Coords {rowVal::Int, columnVal::Int, count::Char} deriving (Show, Read)
 --plansza = [Coords]
 --plansza2 = [((Int, Int), Char)]
 
---strToPlansza :: String -> [((Int, Int), Char)]
---strToPlansza [] = []
---strToPlansza (x:xs) = do
---                      xCoor <- 0
---                      yCoor <- 0
---                      plansza <- []
---                      if (x == " ")
---                      then do strToPlansza(xs)
---                      else ((xCoor,yCoor), x):plansza
---                      return plansza
 
 
---strToTab :: String -> [[Char]]
---strToTab [] = []
---strToTab (x:xs) = do
---    line <- 0
---    if (x == "/n")
---      then do tab:[]
---              line = line+1
---    else
---      tab:x
---  return tab
+
+checkWords [] [] = []
+checkWords planszaTab slowaTab = do
+    let xC = 0
+    let yC = 0
+    let letter = slowaTab !! xC !! yC
+    let row = 0
+    let planszaRowsNum = length planszaTab
+    let foundIndexes = [elemIndices letter (planszaTab !! row) | row <- [0..planszaRowsNum-1]]
+    return foundIndexes
 
 main = do
-    putStrLn "File name:"
-    name <- getLine
-    handle <- openFile ("../plikiTestowe/" ++ name) ReadMode
-    combStr <- hGetContents handle
-    let tab = lines combStr
-    return tab
+    putStrLn "Plansza:"
+    --planszaPath <- getLine
+    handlePlansza <- openFile ("../plikiTestowe/" ++ "lam1_plansza.txt") ReadMode
+    putStrLn "Slowa:"
+    --slowaPath <- getLine
+    handleSlowa <- openFile ("../plikiTestowe/" ++ "lam1_slowa.txt") ReadMode
+    handledPlansza <- hGetContents handlePlansza
+    handledSlowa <- hGetContents handleSlowa
+    let tabPlansza = lines handledPlansza
+    let tabSlowa = lines handledSlowa
+    let chW = checkWords tabPlansza tabSlowa
+    return (tabPlansza, tabSlowa, chW)
