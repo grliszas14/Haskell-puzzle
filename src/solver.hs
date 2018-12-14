1a29e6c3a737100a63ecea0bfe694357135944df
gepard5 <michal.glinka95@gmail.com>
2018-12-14 22:45:25 +0100

    Add finding words on letters map

--- src/solver.hs
+++ src/solver.hs
@@ -1,8 +1,44 @@
 import System.IO
 import Data.List
 
--- zwraca ( (x, y), orientacja )
-find_word :: String -> [ [ Char ] ] -> ( (Int, Int), Int )
+
+data Orientation = Vert | Hori | Diag | None deriving (Enum, Eq, Show)
+
+get_horizontal_list::[[Char]]->Int->Int->[(Int, Int)]
+get_horizontal_list all_letters x y = if (x < (length all_letters)) then (x,y):(get_horizontal_list all_letters (x+1) y) else []
+
+get_vertical_list::[[Char]]->Int->Int->[(Int, Int)]
+get_vertical_list all_letters x y = if (y < (length all_letters)) then (x,y):(get_vertical_list all_letters x (y+1)) else []
+
+get_diagonal_list::[[Char]]->Int->Int->[(Int, Int)]
+get_diagonal_list all_letters x y = if (x < (length all_letters)) then (x,y):(get_diagonal_list all_letters (x+1) (y+1)) else []
+
+get_word_from_list::[[Char]]->[(Int, Int)]->[Char]
+get_word_from_list all_letters ((x, y):coords) = (all_letters !! x !! y ):(get_word_from_list all_letters coords)
+get_word_from_list _ [] = []
+
+begins_with :: [ Char ] -> [ Char ] -> Bool
+begins_with (a:word) (b:prefix) = if (a == b) then (begins_with word prefix) else False
+begins_with [] _ = True
+begins_with _ [] = False
+
+
+is_word_in_place :: [ [ Char ] ] -> Int -> Int -> [ Char ] -> Orientation
+is_word_in_place all_letters x y word
+    | begins_with word ( get_word_from_list all_letters (get_horizontal_list all_letters x y ) ) = Hori
+    | begins_with word ( get_word_from_list all_letters (get_vertical_list all_letters x y) ) = Vert
+    | begins_with word ( get_word_from_list all_letters (get_diagonal_list all_letters x y) ) = Diag
+    | otherwise = None
+
+get_next :: [ [ Char ] ] -> (Int, Int) -> (Int, Int)
+get_next all_letters (x, y)
+    | x < (length all_letters) = ((x+1), y)
+	| y < (length all_letters) = (0, (y+1))
+    | otherwise  = (-1, -1)
+
+
+find_word :: String -> [ [ Char ] ] -> (Int, Int)-> ( (Int, Int), Orientation )
+find_word word all_letters (x, y) = if ((is_word_in_place all_letters x y word)==None) then (find_word word all_letters (get_next all_letters (x, y)) ) else ( (x, y), (is_word_in_place all_letters x y word))
 
 get_removed_list :: String -> Int -> [ [ Char ] ] -> [ [ Char ] ]
 
