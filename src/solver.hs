041cde7eb0d42e05040a0fdf86596e201eae7e01
Michal Glinka <michal.glinka95@gmail.com>
2019-01-13 18:23:25 +0100

    Final rework

--- src/solver.hs
+++ src/solver.hs
@@ -2,26 +2,19 @@ import System.IO
 import Data.List
 
 
-data Orientation = Vert | Hori | Diag | Updi | Upup | None deriving (Enum, Eq, Show)
+data Orientation = Vert | Hori | Diag | Updi | None deriving (Enum, Eq, Show)
 
-get_horizontal_list::[[Char]]->Int->Int->[(Int, Int)]
-get_horizontal_list all_letters x y = if (x < (length (head all_letters))) then (x,y):(get_horizontal_list all_letters (x+1) y) else []
+get_horizontal_list::[[Char]]->Int->Int->[Char]
+get_horizontal_list all_letters x y = if (x < (length (head all_letters))) then (all_letters !! y !! x):(get_horizontal_list all_letters (x+1) y) else []
 
-get_vertical_list::[[Char]]->Int->Int->[(Int, Int)]
-get_vertical_list all_letters x y = if (y < (length all_letters)) then (x,y):(get_vertical_list all_letters x (y+1)) else []
+get_vertical_list::[[Char]]->Int->Int->[Char]
+get_vertical_list all_letters x y = if (y < (length all_letters)) then (all_letters !! y !! x):(get_vertical_list all_letters x (y+1)) else []
 
-get_diagonal_list::[[Char]]->Int->Int->[(Int, Int)]
-get_diagonal_list all_letters x y = if ( y < (length all_letters) && x < (length (head all_letters)) ) then (x,y):(get_diagonal_list all_letters (x+1) (y+1)) else []
+get_diagonal_list::[[Char]]->Int->Int->[Char]
+get_diagonal_list all_letters x y = if ( y < (length all_letters) && x < (length (head all_letters)) ) then (all_letters !! y !! x):(get_diagonal_list all_letters (x+1) (y+1)) else []
 
-get_up_list::[[Char]]->Int->Int->[(Int, Int)]
-get_up_list all_letters x y = if ( y >= 0 && x < (length (head all_letters)) ) then (x,y):(get_up_list all_letters (x+1) (y-1)) else []
-
-get_upup_list::[[Char]]->Int->Int->[(Int, Int)]
-get_upup_list all_letters x y = if ( y >= 0 ) then (x,y):(get_upup_list all_letters x (y-1)) else []
-
-get_word_from_list::[[Char]]->[(Int, Int)]->[Char]
-get_word_from_list all_letters ((x, y):coords) = (all_letters !! y !! x ):(get_word_from_list all_letters coords)
-get_word_from_list _ [] = []
+get_up_list::[[Char]]->Int->Int->[Char]
+get_up_list all_letters x y = if ( y >= 0 && x < (length (head all_letters)) ) then (all_letters !! y !! x):(get_up_list all_letters (x+1) (y-1)) else []
 
 begins_with :: [ Char ] -> [ Char ] -> Bool
 begins_with (a:word) (b:prefix) = if (a == b) then (begins_with word prefix) else False
@@ -31,11 +24,10 @@ begins_with _ [] = False
 
 is_word_in_place :: [ [ Char ] ] -> Int -> Int -> [ Char ] -> Orientation
 is_word_in_place all_letters x y word
-    | begins_with word ( get_word_from_list all_letters (get_horizontal_list all_letters x y ) ) = Hori
-    | begins_with word ( get_word_from_list all_letters (get_vertical_list all_letters x y) ) = Vert
-    | begins_with word ( get_word_from_list all_letters (get_diagonal_list all_letters x y) ) = Diag
-    | begins_with word ( get_word_from_list all_letters (get_up_list all_letters x y) ) = Updi
-    | begins_with word ( get_word_from_list all_letters (get_upup_list all_letters x y) ) = Upup
+    | begins_with word ( get_horizontal_list all_letters x y ) = Hori
+    | begins_with word ( get_vertical_list all_letters x y) = Vert
+    | begins_with word ( get_diagonal_list all_letters x y) = Diag
+    | begins_with word ( get_up_list all_letters x y) = Updi
     | otherwise = None
 
 get_next :: [ [ Char ] ] -> (Int, Int) -> (Int, Int)
@@ -50,7 +42,10 @@ find_words word all_letters (x, y) = if ((find_word word all_letters (x, y)) == 
 
 find_word :: String -> [ [ Char ] ] -> (Int, Int)-> ( (Int, Int), Orientation )
 find_word _ _ (-1, -1) = ( (-1, -1), None )
-find_word word all_letters (x, y) = if ((is_word_in_place all_letters x y word)==None) then (find_word word all_letters (get_next all_letters (x, y)) ) else ( (x, y), (is_word_in_place all_letters x y word))
+find_word word all_letters (x, y)
+  | orien == None = (find_word word all_letters (get_next all_letters (x, y)))
+  | otherwise = ( (x, y), orien )
+  where orien = (is_word_in_place all_letters x y word)
 
 get_n_elems :: [t] -> Int -> [t]
 get_n_elems _ 0 = []
@@ -67,7 +62,6 @@ orientation_to_list ( (x, y),orien ) len
   | orien == Vert = ( (x, y):(orientation_to_list ( (x, (y+1)), Vert) (len-1) ) )
   | orien == Diag = ( (x, y):(orientation_to_list ( ((x+1), (y+1)), Diag) (len-1) ) )
   | orien == Updi = ( (x, y):(orientation_to_list ( ((x+1), (y-1)), Updi) (len-1) ) )
-  | orien == Upup = ( (x, y):(orientation_to_list ( (x, (y-1)), Upup) (len-1) ) )
   | otherwise = []
 
 is_in_list :: (Int, Int) -> [ (Int, Int) ] -> Bool
@@ -114,6 +108,7 @@ main = do
     handledWords <- hGetContents handleWords
     let tabBoard = lines handledBoard
     let tabWords = lines handledWords
+    putStrLn "Plansza wczytana"
     let tabPlanszaRozwiazana = get_unused_letters_impl tabBoard tabWords
     putStrLn "\n"
     putStrLn "Plansza początkowa:"
